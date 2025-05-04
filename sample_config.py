b3e5e9abf1efcd78c0b795b16f331b6f89b6e106
garlapatiraviteja <garlapatiravitejag.gr@gmail.com>
2025-05-04 14:16:54 +0530

    adding the config file

---
+++ sample_config.py
@@ -0,0 +1,5 @@
+
+# config.py
+DEEPSEEK_API_KEY = "USE_API_KEY"
+MODEL_PATH = "phi-3-mini-4k-instruct.Q4_K_M"  # download the model from internet preferely .gguf
+FALLBACK_MODEL_PATH = "mistral-7b-instruct-v0.2.Q3_K_S" # download the model from internet preferely .gguf
